84cd20b9acd0ae74236a0315a5f24908b2824e1b
zeeshan <itexpert120@gmail.com>
2021-01-04 20:31:17 +0500

    j5x-common: add more tweaks to system_prop.mk

--- system_prop.mk
+++ system_prop.mk
@@ -85,3 +85,107 @@ PRODUCT_PROPERTY_OVERRIDES += \
         dalvik.vm.heapminfree=512k \
         dalvik.vm.heapmaxfree=8m
 
+# Disable Nav Bar
+PRODUCT_PROPERTY_OVERRIDES += \
+	qemu.hw.mainkeys=1
+
+# Network
+PRODUCT_PROPERTY_OVERRIDES += \
+	net.tcp.buffersize.default=4096,87380,256960,4096, 16384,256960 \
+	net.tcp.buffersize.wifi=4096,87380,256960,4096,163 84,256960 \
+	net.tcp.buffersize.lte=4096,87380,256960,4096,163 84,256960 \
+	net.tcp.buffersize.gprs=4096,87380,256960,4096,163 84,256960 \
+	net.tcp.buffersize.edge=4096,87380,256960,4096,163 84,256960 \
+	net.dns1=8.8.8.8 \
+	net.dns2=8.8.4.4 \
+	net.rmnet0.dns1=8.8.8.8 \
+	net.rmnet0.dns2=8.8.4.4
+
+# Better Scrolling
+PRODUCT_PROPERTY_OVERRIDES += \
+	windowsmgr.max_events_per_sec=150 \
+	ro.min_pointer_dur=8 \
+	ro.max.fling_velocity=12000 \
+	ro.min.fling_velocity=8000 \
+	persist.sys.scrollingcache=3 \
+	debug.sf.hw=1
+
+# Call Tweaks
+PRODUCT_PROPERTY_OVERRIDES += \
+	mot.proximity.delay=0 \
+	ro.lge.proximity.delay=25 \
+	ro.telephony.call_ring.delay=0 \
+	ring.delay=0 \
+	ro.ril.enable.amr.wideband=1 \
+	ro.config.vc_call_steps=20
+
+# Faster Booting
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.config.hw_quickpoweron=true
+
+# Wifi Tweaks
+PRODUCT_PROPERTY_OVERRIDES += \
+	wifi.supplicant_scan_interval=120
+
+# Gaming Tweaks
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.sys.NV_FPSLIMIT=60 \
+	persist.sys.NV_POWERMODE=1 \
+	persist.sys.NV_PROFVER=15 \
+	persist.sys.NV_STEREOCTRL=0 \
+	persist.sys.NV_STEREOSEPCHG=0 \
+	persist.sys.NV_STEREOSEP=20 \
+	persist.sys.purgeable_assets=1 \
+	debug.enabletr=true \
+	debug.qctwa.preservebuf=1 \
+	dev.pm.dyn_samplingrate=1 \
+	video.accelerate.hw=1 \
+	ro.vold.umsdirtyratio=20 \
+	debug.overlayui.enable=1 \
+	debug.egl.hw=1 \
+	ro.fb.mode=1 \
+	hw3d.force=1 \
+	persist.sys.ui.hw=1 \
+	ro.sf.compbypass.enable=0 \
+	debug.sf.hw=1 \
+	debug.composition.type=c2d \
+	persist.sys.composition.type=c2d \
+	debug.performance.tuning=1 \
+	ro.media.dec.jpeg.memcap=8000000 \
+	ro.media.enc.hprof.vid.bps=8000000 \
+	ro.media.dec.aud.wma.enabled=1 \
+	ro.media.dec.vid.wmv.enabled=1 \
+	ro.media.cam.preview.fps=0 \
+	ro.media.codec_priority_for_thumb=so
+
+# Smoother Experince
+PRODUCT_PROPERTY_OVERRIDES += \
+	debug.sf.hw=1 \
+	persist.sys.ui.hw=1 \
+	debug.performance.tuning=1 \
+	video.accelerate.hw=1 \
+	debug.egl.profiler=1 \
+	debug.egl.hw=1 \
+	debug.composition.type=gpu
+
+# Faster Video Steams
+PRODUCT_PROPERTY_OVERRIDES += \
+	media.stagefright.enable-player=true \
+	media.stagefright.enable-meta=true \
+	media.stagefright.enable-scan=true \
+	media.stagefright.enable-http=true \
+	media.stagefright.enable-rtsp=true \
+	media.stagefright.enable-record=true
+
+# Network Speed
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.ril.hsxpa=2 \
+	ro.ril.gprsclass=10 \
+	ro.ril.hep=1 \
+	ro.ril.enable.dtm=1 \
+	ro.ril.hsdpa.category=10 \
+	ro.ril.enable.a53=1 \
+	ro.ril.enable.3g.prefix=1 \
+	ro.ril.htcmaskw1.bitmask=4294967295 \
+	ro.ril.htcmaskw1=14449 \
+	ro.ril.hsupa.category=5 \

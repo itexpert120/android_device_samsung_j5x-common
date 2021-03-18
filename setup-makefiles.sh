662a189a76f48327921529cbec70560803f5d39c
zeeshan <itexpert120@gmail.com>
2021-03-18 09:29:41 +0500

    j5x-common: Switch to standalone extract utils

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -8,7 +8,7 @@
 
 set -e
 
-export DEVICE_COMMON=sdm660-common
+export DEVICE_COMMON=msm8916-common
 export DEVICE_SPECIFIED_COMMON_DEVICE="j5xnlte j5xlte"
 
 "./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
